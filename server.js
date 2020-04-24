d05d76319c4128aea82efa17e39b28112d883547
Murad Cholukov <muradusa@gmail.com>
2020-04-24 07:58:38 -0700

    initial set  up of server.js complete

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express")
+var logger = require("morgan")
+var mongoose = require("mongoose")
+
+var axios = require("axios")
+var cheerio = require("cheerio")
+
+//var db = require ("./models")
+
+var PORT = 3000
+
+var app = express()
+
+app.use(logger("dev"))
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
+app.use(express.static("public"))
+
+mongoose.connect("mongodb://localhost/mongoose", { useNewUrlParser: true })
+
+// Here I will need to link my routes
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + " !")
+})
