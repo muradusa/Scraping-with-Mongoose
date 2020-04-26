6bc32b184e2aee4ac1548b76d5355ae95c2eef15
Murad Cholukov <muradusa@gmail.com>
2020-04-26 16:12:18 -0700

    adding empty models and routes folders

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose")
 var axios = require("axios")
 var cheerio = require("cheerio")
 
-//var db = require ("./models")
+// var db = require ("./models")
 
 var PORT = 3000
 
